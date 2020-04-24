3bf1c0dd20023e2f917187366d247a54bbedc5fe
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-04-24 15:02:15 +0200

    Clean up sierpinski carpet code

--- sketch.js
+++ sketch.js
@@ -1,19 +1,37 @@
-const len = window.length;
+let length = 0;
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(window.innerWidth, window.innerHeight);
+  if (window.innerWidth > window.innerHeight) {
+    length = window.innerHeight * 0.8;
+  } else {
+    length = window.innerWidth * 0.8;
+  }
 }
 
-function branch(len) {
-  fill(random(255), random(255), random(255));
-  rect(0, 0, len, len);
-  if (len > 1) {
-    branch(len * 0.99);
+function SierpinskiCarpet(len, coordinates, iterations) {
+  fill(0);
+  stroke(255);
+  rect(coordinates.x, coordinates.y, len, len);
+  for (let x = 0; x <= 2; x++) {
+    for (let y = 0; y <= 2; y++) {
+      if ((x === 1 && y === 1) || iterations === 6) {
+      } else {
+        const newCoordinates = {
+          x: coordinates.x + x * (len / 3),
+          y: coordinates.y + y * (len / 3),
+        };
+        SierpinskiCarpet(len / 3, newCoordinates, iterations + 1);
+      }
+    }
   }
 }
 
 function draw() {
   background(51);
-  translate(width / 2, height / 2);
-  rectMode(CENTER);
-  branch(width);
+  translate(
+    (window.innerWidth - length) / 2,
+    (window.innerHeight - length) / 2
+  );
+  SierpinskiCarpet(length, { x: 0, y: 0 }, 0);
+  noLoop();
 }
