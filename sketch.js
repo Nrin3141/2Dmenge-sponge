e10b378ffac5ad5f883d9dc8fc7943054a4dbdb9
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-04-24 11:42:22 +0200

    Clean out draw from setup

--- sketch.js
+++ sketch.js
@@ -1,18 +1,19 @@
-var len;
-var x = 255;
+const len = window.length;
 function setup() {
-  var canvas = createCanvas(800, 800);
-  }
-  function draw() {
-    background(51);
-    translate(width/2, height/2);
-    rectMode(CENTER);
-    branch(width);
-    }
+  createCanvas(800, 800);
+}
+
 function branch(len) {
-    fill(random(255), random(255), random(255));
-    rect(0, 0, len, len);
-    if (len > 1) {
-      branch(len * 0.99);
+  fill(random(255), random(255), random(255));
+  rect(0, 0, len, len);
+  if (len > 1) {
+    branch(len * 0.99);
   }
+}
+
+function draw() {
+  background(51);
+  translate(width / 2, height / 2);
+  rectMode(CENTER);
+  branch(width);
 }
